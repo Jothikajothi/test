28dc181f3f564abb7de998a39787d13a5f12516a
Jothikajothi <devloperjothikajothi@gmail.com>
2022-06-24 10:08:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+ 
+// our CustomFunction ("js") 
+// let x=11;
+// if(x==10){
+//   alert("x is equale 10")  
+// }else{
+//     alert("x is not 19")
+// }
+// var x=17;
+// if(x==16){
+// console.log("x is possitive")
+// }else{
+//     console.log("x is negative")
+// }
+// var x=10;
+// if(x==10){
+//    document.write ("x is true")
+// }else{
+//     document.write("x is false")
+// }
+// let x=20;
+// if(x==11 ){
+//     document.write("x is 20")
+// } else if(x==20){
+//     document.write("x is not20")
+// } 
+// let e=19;
+// if(e==12){
+//    document.write("e is not 19") 
+// }else if(e==10){
+//     document.write("e is 19")
+// }else{
+//     document.write("x is not 10 or x is not 19 or x is not 12")
+// }
+//and operator &&:true *true=true,false * true=false,true *false=false,false *false =false
+
+// var y= 8;
+// if(y <10 && y>9){
+//     document.write("the number is  wrong")
+// }else if( y>3 && y<10){
+//    document.write("the number is right") 
+// }else{
+//     document.write("the number is not match")
+// }
+
+  var x=30;
+//  x==20 ? document.write (" x is 30"):document.write("x is not 30");
+// var y= x > 29 ?? "the number is right";
+//  document.write(y);
+// var y= x==20 ??" the number is match-maching";
+//  if (y==true){
+//      document.write("x number is  not right")
+//  }else if(x>20) {
+//    document.write("the number is right")  
+//  }else{
+//      document.write("the number is  justife")
+//  }
+
+// var y = 40;
+// y > 50 ? document.write ("y is not 40"): (y>60 ? document.write("x is not 60"):document.write ("x is not 50 or not 60"));
+
+// ///////*****        Loop     */
+
+// for (x=1;x<=100;x=x+1){
+//     document.write(x);
+// }
+// for (x=1;x<=100;x=x+1){
+//   console.log(x);
+// }
+// b=2;
+// while(b<99){
+//     console.log(b)
+//     b=b+1;
+// }
+y=1;
+while (y<30){
+    console.log(y);
+    y=y+2;
+}
+
+
+ 
